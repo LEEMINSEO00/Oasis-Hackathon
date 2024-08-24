a7fa10ab18705a8e71f4db0b81cd7956411945f5
LEEMINSEO <real2ms@naver.com>
2024-08-24 20:49:35 +0900

    feat: elder login

---
+++ app/src/main/java/com/example/oasis_hackathon/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.oasis_hackathon
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.elder_login)
+    }
+}
