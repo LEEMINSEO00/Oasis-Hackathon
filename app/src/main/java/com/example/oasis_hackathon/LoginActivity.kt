3892424040ef922a55277f9d25192a12bc1d1102
LEEMINSEO <real2ms@naver.com>
2024-08-25 20:22:44 +0900

    feat: move login to main

---
+++ app/src/main/java/com/example/oasis_hackathon/LoginActivity.kt
@@ -0,0 +1,19 @@
+package com.example.oasis_hackathon
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.elder_login)
+
+        val elderLoginButton = findViewById<ImageButton>(R.id.elder_kakaoLogin)
+        elderLoginButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
--- app/src/main/java/com/example/oasis_hackathon/MainActivity.kt
+++ app/src/main/java/com/example/oasis_hackathon/MainActivity.kt
@@ -3,9 +3,9 @@ package com.example.oasis_hackathon
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity  : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.elder_write)
+        setContentView(R.layout.elder_main)
     }
 }
