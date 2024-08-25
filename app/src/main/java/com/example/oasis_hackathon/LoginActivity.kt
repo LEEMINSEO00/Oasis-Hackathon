0018c5da03459ed4c93a4cc6be3f8bb970e8ca3f
LEEMINSEO <real2ms@naver.com>
2024-08-25 20:41:15 +0900

    feat: elder finish write method

--- app/src/main/java/com/example/oasis_hackathon/LoginActivity.kt
+++ app/src/main/java/com/example/oasis_hackathon/LoginActivity.kt
@@ -14,6 +14,7 @@ class LoginActivity : AppCompatActivity() {
         elderLoginButton.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
+            finish()
         }
     }
 }
--- app/src/main/java/com/example/oasis_hackathon/WriteActivity.kt
+++ app/src/main/java/com/example/oasis_hackathon/WriteActivity.kt
@@ -1,11 +1,20 @@
 package com.example.oasis_hackathon
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
 class WriteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.elder_write)
+
+        val completeLongButton = findViewById<ImageButton>(R.id.completeLong)
+        completeLongButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
