a9b13a055de0b53bdf0a1f43e53d9d468331b8e4
LEEMINSEO <real2ms@naver.com>
2024-08-25 20:29:30 +0900

    feat: move main to record, camera, write method

--- app/src/main/java/com/example/oasis_hackathon/MainActivity.kt
+++ app/src/main/java/com/example/oasis_hackathon/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.oasis_hackathon
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity  : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.elder_main)
+
+        val recordMethodButton = findViewById<ImageButton>(R.id.elder_recordMethod)
+        recordMethodButton.setOnClickListener {
+            val intent = Intent(this, RecordActivity::class.java)
+            startActivity(intent)
+        }
+
+        val cameraMethodButton = findViewById<ImageButton>(R.id.elder_cameraMethod)
+        cameraMethodButton.setOnClickListener {
+            val intent = Intent(this, CameraActivity::class.java)
+            startActivity(intent)
+        }
+
+        val writeMethodButton = findViewById<ImageButton>(R.id.elder_writeMethod)
+        writeMethodButton.setOnClickListener {
+            val intent = Intent(this, WriteActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
