9ef1475fe166f547489b0e3c7799f64a764a5540
LEEMINSEO <real2ms@naver.com>
2024-08-27 03:17:00 +0900

    feat: protector heads-up notification

--- app/src/main/java/com/example/oasis_hackathon/protector/ProMainActivity.kt
+++ app/src/main/java/com/example/oasis_hackathon/protector/ProMainActivity.kt
@@ -1,21 +1,107 @@
 package com.example.oasis_hackathon.protector
 
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.example.oasis_hackathon.R
 
 class ProMainActivity : AppCompatActivity() {
+
+    private val CHANNEL_ID = "my_channel_id"
+    private val NOTIFICATION_ID = 100
+    private val REQUEST_CODE_POST_NOTIFICATION = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.protector_notification)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.POST_NOTIFICATIONS
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+                    REQUEST_CODE_POST_NOTIFICATION
+                )
+            } else {
+                createNotificationChannel()
+                showNotification()
+            }
+        } else {
+            createNotificationChannel()
+            showNotification()
+        }
 
         val negativeNotification = findViewById<LinearLayout>(R.id.negativeNotification)
 
         negativeNotification.setOnClickListener {
             val intent = Intent(this, ProAnalysisActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "High Priority Channel"
+            val descriptionText = "This is a high priority channel for Heads-Up Notification"
+            val importance = NotificationManager.IMPORTANCE_HIGH  // 중요도 높게 설정
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    private fun showNotification() {
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.danger)
+            .setContentTitle("오늘의 알림에서 위험이 감지되었어요!")
+            .setContentText("김길동님의 일기에서 ⚠\uFE0F\uFE0F부정적인 단어⚠\uFE0F\uFE0F가 감지되었어요\uD83E\uDD72 얼른 확인하세요!")
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setDefaults(NotificationCompat.DEFAULT_ALL)
+            .setAutoCancel(true)
+            .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+
+        with(NotificationManagerCompat.from(this)) {
+            if (ActivityCompat.checkSelfPermission(
+                    this@ProMainActivity,
+                    Manifest.permission.POST_NOTIFICATIONS
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                notify(NOTIFICATION_ID, builder.build())
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == REQUEST_CODE_POST_NOTIFICATION) {
+            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                createNotificationChannel()
+                showNotification()
+            }
         }
     }
 }
