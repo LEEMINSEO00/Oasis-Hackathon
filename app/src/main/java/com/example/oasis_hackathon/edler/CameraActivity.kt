7a2b037b41185383dd45518f1552c95c9e5ae8a9
LEEMINSEO <real2ms@naver.com>
2024-08-26 17:47:31 +0900

    feat: delete useless comments

--- app/src/main/java/com/example/oasis_hackathon/edler/CameraActivity.kt
+++ app/src/main/java/com/example/oasis_hackathon/edler/CameraActivity.kt
@@ -20,20 +20,17 @@ class CameraActivity : AppCompatActivity() {
 
         photoExImageView = findViewById(R.id.photoEx)
 
-        // 처음 Activity가 열렸을 때 intent로 전달된 이미지 표시
         val bitmap = intent.getParcelableExtra<Bitmap>("imageBitmap")
         if (bitmap != null) {
             photoExImageView.setImageBitmap(bitmap)
             photoExImageView.visibility = ImageView.VISIBLE
         }
 
-        // recamera 버튼 클릭 시 카메라 앱을 다시 호출
         val recameraButton = findViewById<ImageButton>(R.id.recamera)
         recameraButton.setOnClickListener {
-            callCamera()  // 카메라 앱 실행
+            callCamera()
         }
 
-        // cameraComplete 버튼 클릭 시 다른 화면으로 이동
         val cameraCompleteButton = findViewById<ImageButton>(R.id.cameraComplete)
         cameraCompleteButton.setOnClickListener {
             val intent = Intent(this, CameraProgressActivity::class.java)
@@ -41,20 +38,17 @@ class CameraActivity : AppCompatActivity() {
         }
     }
 
-    // 카메라 호출 메서드
     private fun callCamera() {
         val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
         startActivityForResult(cameraIntent, CAMERA_CODE)
     }
 
-    // 카메라 결과 처리
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
         if (resultCode == RESULT_OK && requestCode == CAMERA_CODE) {
             val bitmap = data?.extras?.get("data") as? Bitmap
             bitmap?.let {
-                // 찍은 사진을 photoEx에 표시
                 photoExImageView.setImageBitmap(bitmap)
                 photoExImageView.visibility = ImageView.VISIBLE
             }
