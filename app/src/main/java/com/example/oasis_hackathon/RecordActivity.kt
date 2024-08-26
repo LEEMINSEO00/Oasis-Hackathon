55c6d63847a06933aebcd8c642819ed6ddb40701
LEEMINSEO <real2ms@naver.com>
2024-08-26 15:35:09 +0900

    feat: elder recorded text

--- app/src/main/java/com/example/oasis_hackathon/RecordActivity.kt
+++ app/src/main/java/com/example/oasis_hackathon/RecordActivity.kt
@@ -2,7 +2,10 @@ package com.example.oasis_hackathon
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.ImageButton
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class RecordActivity : AppCompatActivity() {
@@ -15,12 +18,17 @@ class RecordActivity : AppCompatActivity() {
 
         val recordStartButton = findViewById<ImageButton>(R.id.recordStart)
         val recordFinishButton = findViewById<ImageButton>(R.id.recordFinish)
+        val delayedTextView = findViewById<TextView>(R.id.recordedText)
 
         recordStartButton.setOnClickListener {
             if (!isRecording) {
                 recordStartButton.setImageResource(R.drawable.record_pause)
                 recordFinishButton.setImageResource(R.drawable.record_finish_gray)
                 isRecording = true
+
+                Handler(Looper.getMainLooper()).postDelayed({
+                    delayedTextView.visibility = TextView.VISIBLE
+                }, 1000)
             } else {
                 recordStartButton.setImageResource(R.drawable.record_start)
                 recordFinishButton.setImageResource(R.drawable.record_finish)
