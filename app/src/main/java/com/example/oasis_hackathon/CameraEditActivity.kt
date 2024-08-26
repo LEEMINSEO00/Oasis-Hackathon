6d4258ed34c6c32fce9820b5c094cd2c0ca7a3ea
LEEMINSEO <real2ms@naver.com>
2024-08-26 14:41:53 +0900

    feat: elder recamera&complete camera

---
+++ app/src/main/java/com/example/oasis_hackathon/CameraEditActivity.kt
@@ -0,0 +1,27 @@
+package com.example.oasis_hackathon
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+
+class CameraEditActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.elder_camera_edit)
+
+        val rerecordButton = findViewById<ImageButton>(R.id.recamera)
+        rerecordButton.setOnClickListener {
+            val intent = Intent(this, CameraActivity::class.java)
+            startActivity(intent)
+        }
+
+        val recordCompleteButton = findViewById<ImageButton>(R.id.cameraComplete)
+        recordCompleteButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(intent)
+            finish()
+        }
+    }
+}
