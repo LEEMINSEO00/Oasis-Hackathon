941fd2e4e2c6b95a2a77cebd823c9f392b33372e
LEEMINSEO <real2ms@naver.com>
2024-08-26 14:31:49 +0900

    feat: elder rerecord&complete record

---
+++ app/src/main/java/com/example/oasis_hackathon/RecordEditActivity.kt
@@ -0,0 +1,27 @@
+package com.example.oasis_hackathon
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+
+class RecordEditActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.elder_record_edit)
+
+        val rerecordButton = findViewById<ImageButton>(R.id.rerecord)
+        rerecordButton.setOnClickListener {
+            val intent = Intent(this, RecordActivity::class.java)
+            startActivity(intent)
+        }
+
+        val recordCompleteButton = findViewById<ImageButton>(R.id.recordComplete)
+        recordCompleteButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(intent)
+            finish()
+        }
+    }
+}
