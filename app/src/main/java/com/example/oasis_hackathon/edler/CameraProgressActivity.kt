8437923aa97ec7fbc60b41f7d4e39d581c3915ad
LEEMINSEO <real2ms@naver.com>
2024-08-26 17:51:54 +0900

    feat: elder move to camera edit page after camera progress

--- app/src/main/java/com/example/oasis_hackathon/edler/CameraProgressActivity.kt
+++ app/src/main/java/com/example/oasis_hackathon/edler/CameraProgressActivity.kt
@@ -1,6 +1,9 @@
 package com.example.oasis_hackathon.edler
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import com.example.oasis_hackathon.R
 
@@ -9,5 +12,11 @@ class CameraProgressActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.elder_camera_progress)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, CameraEditActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
     }
 }
